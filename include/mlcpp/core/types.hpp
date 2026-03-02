0d72c157cfd8a453ce09279e1e8ad2f07765abfb
Liam Ellison <liam.ellison.codes@gmail.com>
2026-03-01 22:04:16 -0500

    init setup + types

---
+++ include/mlcpp/core/types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace mlcpp {
+using Vec = Eigen::VectorXd;
+using Mat = Eigen::MatrixXd;
+using IVec = Eigen::VectorXi; // integer labels
+using RowVec = Eigen::RowVectorXd;
+} // namespace mlcpp
